a0ae1c0ca2519731e0a99d5253f4b1bc806e227a
Oskar Freye <oskar@freye.tech>
2026-03-09 16:17:37 +0100

    green: wire audit panel into content script

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,5 +1,7 @@
 import { initPageDetection } from './page-detection';
 import { initCategoryBadges } from './category-display';
+import { initAuditPanel } from './audit-panel';
 
 const cleanupPageDetection = initPageDetection();
 const cleanupCategoryBadges = initCategoryBadges();
+const cleanupAuditPanel = initAuditPanel();
